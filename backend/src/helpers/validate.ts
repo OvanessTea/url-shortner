d68efab4e4281ef1ffb365aae919e4114a6b3929
OvanessTea <OvanessTea@yandex.com>
2025-06-23 14:51:37 +0200

    add error throwing in validator

--- backend/src/helpers/validate.ts
+++ backend/src/helpers/validate.ts
@@ -1,9 +1,10 @@
 import { validate } from 'class-validator';
 import { Response } from 'express';
+import BadRequestError from '../errors/bad-request-error';
 
 export async function validateDto(dto: object, res: Response) {
   const errors = await validate(dto);
   if (errors.length > 0) {
-    return res.status(400).json({ message: errors[0].toString() });
+    throw new BadRequestError(errors[0].toString());
   }
 }
