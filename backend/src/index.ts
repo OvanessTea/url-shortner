3fdc2e4e32dd0b2e913630e6a731e7769a293712
OvanessTea <OvanessTea@yandex.com>
2025-06-23 11:54:41 +0200

    integrate DB connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,32 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 dotenv.config();
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT as string),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: true,
+    logging: false,
+    // entities: [Url, Click],
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Express server started on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.error('TypeORM connection error:', error)); 
 
 export default app;
