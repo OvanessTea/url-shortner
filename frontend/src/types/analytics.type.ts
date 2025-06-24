e750006b196400919393798a99295a2d95c38e8d
OvanessTea <OvanessTea@yandex.com>
2025-06-24 16:44:38 +0200

    init types

---
+++ frontend/src/types/analytics.type.ts
@@ -0,0 +1,5 @@
+export type Analytics = {
+    shortUrl: string;
+    clickCount: number;
+    recentIps: string[];
+}
---
+++ frontend/src/types/url.type.ts
@@ -0,0 +1,8 @@
+export type Url = {
+    originalUrl: string;
+    shortUrl: string;
+    alias?: string;
+    createdAt: string;
+    clickCount: number;
+    expiresAt?: string;
+}
