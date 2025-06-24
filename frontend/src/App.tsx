de74c7b49dadefce00e9862a64acbffb4a770cde
OvanessTea <OvanessTea@yandex.com>
2025-06-24 16:45:38 +0200

    Add UI for UrlForm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { UrlForm } from './components/UrlForm';
 
 export default function App() {
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
   return (
     <div>
       <h1>URL Shortener</h1>
+
+      {error && <div className="error">{error}</div>}
+      {success && <div className="success">{success}</div>}
+
+      <UrlForm setError={setError} setSuccess={setSuccess} />
     </div>
   );
 }
